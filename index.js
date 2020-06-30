125fa2d3f22dfc481763bc2ffde399c21749f4e9
Andrei <mr.dzugan@gmail.com>
2020-06-30 15:55:35 +0300

    Implement tasks on switch-case & additional tasks
    - Documented functions by js-docs

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // =========== IF-ELSE
 
 // #1
-
+/**
+ *
+ * @param {number} a
+ */
 function task1(a) {
     if (a === 0) {
         console.log('Верно');
@@ -11,7 +14,10 @@ function task1(a) {
 }
 
 // #2
-
+/**
+ *
+ * @param {number} a
+ */
 function task2(a) {
     if (a > 0) {
         console.log('Верно');
@@ -21,7 +27,10 @@ function task2(a) {
 }
 
 // #3
-
+/**
+ *
+ * @param {number} a
+ */
 function task3(a) {
     if (a < 0) {
         console.log('Верно');
@@ -31,7 +40,10 @@ function task3(a) {
 }
 
 // #4
-
+/**
+ *
+ * @param {number} a
+ */
 function task4(a) {
     if (a >= 0) {
         console.log('Верно');
@@ -41,7 +53,10 @@ function task4(a) {
 }
 
 // #5
-
+/**
+ *
+ * @param {number} a
+ */
 function task5(a) {
     if (a <= 0) {
         console.log('Верно');
@@ -51,7 +66,10 @@ function task5(a) {
 }
 
 // #6
-
+/**
+ *
+ * @param {number} a
+ */
 function task6(a) {
     if (a !== 0) {
         console.log('Верно');
@@ -61,7 +79,10 @@ function task6(a) {
 }
 
 // #7
-
+/**
+ *
+ * @param {string} a
+ */
 function task7(a) {
     if (a === 'test') {
         console.log('Верно');
@@ -71,7 +92,10 @@ function task7(a) {
 }
 
 // #8
-
+/**
+ *
+ * @param {string} a
+ */
 function task8(a) {
     if (a === '1') {
         console.log('Верно');
@@ -80,12 +104,19 @@ function task8(a) {
     }
 }
 
-// #9
-
+// #1 (#9)
+/**
+ *
+ * @param {boolean} test
+ */
 function task9_1(test) {
     test ? console.log('Верно') : console.log('Неверно');
 }
 
+/**
+ *
+ * @param {boolean} test
+ */
 function task9_2(test) {
     if (test) {
         console.log('Верно');
@@ -94,12 +125,19 @@ function task9_2(test) {
     }
 }
 
-// #10
-
+// #2 (#10)
+/**
+ *
+ * @param {boolean} test
+ */
 function task10_1(test) {
     test ? console.log('Неверно') : console.log('Верно');
 }
 
+/**
+ *
+ * @param {boolean} test
+ */
 function task10_2(test) {
     if (!test) {
         console.log('Верно');
@@ -108,8 +146,11 @@ function task10_2(test) {
     }
 }
 
-// #11
-
+// #1 (#11)
+/**
+ *
+ * @param {number} a
+ */
 function task11(a) {
     if (a > 0 && a < 5) {
         console.log('Верно');
@@ -118,8 +159,11 @@ function task11(a) {
     }
 }
 
-// #12
-
+// #2 (#12)
+/**
+ *
+ * @param {number} a
+ */
 function task12(a) {
     if (a === 0 || a === 2) {
         a += 7;
@@ -129,8 +173,12 @@ function task12(a) {
     alert(a);
 }
 
-// #13
-
+// #3 (#13)
+/**
+ *
+ * @param {number} a
+ * @param {number} b
+ */
 function task13(a, b) {
     if (a <= 1 && b >= 3) {
         console.log(a + b);
@@ -139,12 +187,138 @@ function task13(a, b) {
     }
 }
 
-// #14
-
+// #4 (#14)
+/**
+ *
+ * @param {number} a
+ * @param {number} b
+ */
 function task14(a, b) {
     if ((a > 2 && a < 11) || (b >= 6 && b < 14)) {
         console.log('Верно');
     } else {
         console.log('Неверно');
     }
+}
+
+// =========== SWITCH-CASE
+
+// #1 (#15)
+/**
+ *
+ * @param {number} num
+ */
+function task15(num) {
+    let result;
+    switch (num) {
+        case 1:
+            result = 'Зима';
+            break;
+        case 2:
+            result = 'Весна';
+            break;
+        case 3:
+            result = 'Лето';
+            break;
+        case 4:
+            result = 'Осень';
+            break;
+        default:
+            result = 'Invalid num';
+    }
+    console.log(result);
+}
+
+// =========== TASKS
+
+// #3 (#16)
+/**
+ *
+ * @param {number} day
+ */
+function task16(day) {
+    switch (true) {
+        case (day > 0 && day < 11):
+            console.log('Первая декада');
+            break;
+        case (day > 10 && day < 21):
+            console.log('Вторая декада');
+            break;
+        case (day > 20 && day <= 31):
+            console.log('Третья декада');
+            break;
+        default:
+            console.log('Invalid day');
+    }
+}
+
+// #4 (#17)
+/**
+ *
+ * @param {number} month
+ */
+function task17(month) {
+    switch (true) {
+        case (month > 0 && month < 3) || (month === 12):
+            console.log('Зима');
+            break;
+        case (month > 2 && month < 6):
+            console.log('Весна');
+            break;
+        case (month > 5 && month < 9):
+            console.log('Лето');
+            break;
+        case (month > 8 && month < 12):
+            console.log('Осень');
+            break;
+        default:
+            console.log('Invalid month!');
+    }
+}
+
+// #5 (#18)
+/**
+ *
+ * @param {string} str
+ */
+function task18(str) {
+    str[0] === 'a' ? console.log('Да') : console.log('Нет');
+}
+
+// #6 (#19)
+/**
+ *
+ * @param {string} str
+ */
+function task19(str) {
+    Number(str[0]) > 0 && Number(str[0]) < 4 ? console.log('Да') : console.log('Нет');
+}
+
+// #7 (#20)
+/**
+ *
+ * @param {string} str
+ */
+function task20(str) {
+    let result = 0;
+    for (let i = 0; i < str.length; i++) {
+        result += Number(str[i]);
+    }
+    console.log(result);
+}
+
+// #8 (#21)
+/**
+ *
+ * @param {string} str
+ */
+function task21(str) {
+    let sum1 = 0, sum2 = 0;
+    if (str.length !== 6) {
+        return;
+    }
+    for (let i = 0; i < str.length; i++) {
+        i < 3 ? sum1 += Number(str[i]) : sum2 += Number(str[i]);
+    }
+    sum1 === sum2 ? console.log('Да') : console.log('Нет');
 }
