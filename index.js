0bcea0be91b3bf6bc80e492809aa4dabd6d8fc64
Andrei <mr.dzugan@gmail.com>
2020-06-30 12:53:36 +0300

    Implement tasks on if-else

---
+++ index.js
@@ -0,0 +1,81 @@
+// =========== IF-ELSE
+
+// #1
+
+function task1(a) {
+    if (a === 0) {
+        console.log('Верно');
+    } else {
+        console.log('Неверно');
+    }
+}
+
+// #2
+
+function task2(a) {
+    if (a > 0) {
+        console.log('Верно');
+    } else {
+        console.log('Неверно');
+    }
+}
+
+// #3
+
+function task3(a) {
+    if (a < 0) {
+        console.log('Верно');
+    } else {
+        console.log('Неверно');
+    }
+}
+
+// #4
+
+function task4(a) {
+    if (a >= 0) {
+        console.log('Верно');
+    } else {
+        console.log('Неверно');
+    }
+}
+
+// #5
+
+function task5(a) {
+    if (a <= 0) {
+        console.log('Верно');
+    } else {
+        console.log('Невверно');
+    }
+}
+
+// #6
+
+function task6(a) {
+    if (a !== 0) {
+        console.log('Верно');
+    } else {
+        console.log('Неверно');
+    }
+}
+
+// #7
+
+function task7(a) {
+    if (a === 'test') {
+        console.log('Верно');
+    } else {
+        console.log('Неверно');
+    }
+}
+
+// #8
+
+function task8(a) {
+    if (a === '1') {
+        console.log('Верно');
+    } else {
+        console.log('Неверно');
+    }
+}
